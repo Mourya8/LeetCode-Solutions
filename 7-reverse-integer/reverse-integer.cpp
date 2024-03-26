a9c23b25025f39bfb0dba80785db0f4912c6e07a
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-03-26 10:53:58 -0400

    Time: 5 ms (21.25%) | Memory: 7.4 MB (87.72%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
-        long l= long(INT_MAX);
-        string s=to_string(x);
-        int i=0,j=s.size()-1;
-
-        while(i<j)
-        {
-            swap(s[i],s[j]);
-            i++;
-            j--;
+        int ret = 0;
+        while(x != 0) {
+            if(ret > INT_MAX / 10 || ret < INT_MIN / 10) { return 0; }
+            ret = ret * 10 + x % 10;
+            x/=10;
         }
-
-        //cout <<s<<endl;
-
-        long t= stol(s);
-
-        if(s[s.size()-1]=='-')
-        {
-            t=t*(-1);
-        }
-
-        if (l<t || long(INT_MIN)>t)
-            return 0;
-        
-        return int(t);
+        return ret;
     }
 };
