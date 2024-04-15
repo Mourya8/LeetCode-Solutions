a53e788867a362acc978decf1139e36752944656
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-04-15 17:15:29 -0400

    Time: 81 ms (5.19%) | Memory: 18.2 MB (9.00%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+import queue
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        q = queue.Queue()
+
+        if root==None:
+            return root
+        q.put(root)
+        
+        while(q.qsize()>0):
+            n=q.qsize()
+            for i in range(n):
+                t = q.get()
+                if i==n-1:
+                    t.next=None
+                else:
+                    t.next=q.queue[0]
+                if t.left!=None:
+                    q.put(t.left)
+                    # print(t.val,t.left.val)
+                if t.right!=None:
+                    q.put(t.right)
+                
+
+
+        return root
