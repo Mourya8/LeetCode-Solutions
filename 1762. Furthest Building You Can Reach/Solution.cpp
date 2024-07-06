90671985e063f19d4ba4153e900e573c6b9ab8e9
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-07-06 13:33:19 -0400

    C++ Solution
            Submission Statistics:
            Question #: 1762
            Language: C++
            Runtime: 87 ms
            Runtime percentile: 20.06
            Memory: 57.788 MB
            Memory percentile: 56.64

--- 1762. Furthest Building You Can Reach/Solution.cpp
+++ 1762. Furthest Building You Can Reach/Solution.cpp
@@ -1,63 +1,31 @@
-// class Solution {
-// public:
-//     int furthestBuilding(vector<int>& heights, int b, int l) {
-//         int n=heights.size();
-//         if(n==1)return 0;
-// 	    if(l>=(n-1))return n-1;
-
-//         priority_queue <int,vector<int> , greater<int>> pq;
-//         for(int i=1;i<n;i++)
-//         {
-//             int h = heights[i]-heights[i-1];
-//             if(h<=0) continue;
-//             else if(l>0){
-//                 l--;
-//                 pq.push(h);
-//             }
-//             else if(b>=h)
-//             {
-//                 b-=h;
-//             }
-//             else if(!pq.empty())
-//             {
-//                 int height = pq.top();
-//                 if(b>=height){
-// 		            pq.pop();
-// 		            b-=height;
-// 		            pq.push(h);
-//                 }
-//                 else{
-//                     return i-1;
-//                 }
-
-//             }
-//             else return i-1;
-//         }
-//         return n-1;
-//     }
-// };
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        
-        for (int i = 0; i < heights.size() - 1; ++i) {
-            int diff = heights[i + 1] - heights[i];
-            
-            if (diff > 0) {
-                pq.push(diff);
-                
-                if (pq.size() > ladders) {
-                    int smallest_diff = pq.top();
+    int furthestBuilding(vector<int>& heights, int b, int l) {
+        int n=heights.size();
+        if(n==1)return 0;
+
+        priority_queue <int,vector<int> , greater<int>> pq;
+        for(int i=1;i<n;i++)
+        {
+            int h = heights[i]-heights[i-1];
+            if(h<=0) continue;
+            else if(l>0){
+                l--;
+                pq.push(h);
+            }
+            else 
+            {
+                pq.push(h);
+                int smallest_diff = pq.top();
                     pq.pop();
-                    bricks -= smallest_diff;
+                    b -= smallest_diff;
                     
-                    if (bricks < 0) {
-                        return i;
+                    if (b < 0) {
+                        return i-1;
                     }
-                }
+
             }
         }
-        return heights.size() - 1;
+        return n-1;
     }
 };
