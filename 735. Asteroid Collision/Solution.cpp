b44cfc83aa7f9fda37db8d4afb8ec7e2e28c1dd4
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-07-09 01:35:10 -0400

    C++ Solution
            Submission Statistics:
            Question #: 735
            Language: C++
            Runtime: 51 ms
            Runtime percentile: 6.27
            Memory: 20.676 MB
            Memory percentile: 30.13

---
+++ 735. Asteroid Collision/Solution.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        vector <int> r;
+        for(int i=0;i<asteroids.size();i++)
+        {
+            if(s.empty())
+            {
+                s.push(asteroids[i]);
+            }
+            else
+            {
+                int remain = asteroids[i];
+                if(s.top()>0&&remain<0)
+                {
+                    while(!s.empty()&&(s.top()>0&&remain<0)){
+                        cout<<s.top()<<" "<<remain<<endl;
+                        int temp = s.top();
+                        s.pop();
+
+                        if(remain+temp==0)
+                        {
+                            remain = 0;
+                        }
+                        else if(abs(remain)<abs(temp))
+                        {
+                            remain = temp;
+                        }
+                    }
+                    if(remain!=0)s.push(remain);
+                }
+                else
+                {
+                    s.push(remain);
+                }
+
+            }
+        }
+        while(!s.empty())
+        {
+            int temp=s.top();
+            s.pop();
+            r.push_back(temp);
+        }
+        reverse(r.begin(), r.end());
+        return r;
+    }
+};
