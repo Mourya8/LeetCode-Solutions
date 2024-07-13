7250cd46cd582ea4e7d0f12d4ea578b7a968fdde
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-07-13 19:42:46 -0400

    C++ Solution
            Submission Statistics:
            Question #: 2464
            Language: C++
            Runtime: 51 ms
            Runtime percentile: 38.56
            Memory: 8.196 MB
            Memory percentile: 16.29

--- 2464. Time Needed to Rearrange a Binary String/Solution.cpp
+++ 2464. Time Needed to Rearrange a Binary String/Solution.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
-        int need = 0;
-        int zero = 0;
-        for(char c : s){
-            if(c == '0') zero++;
-            else if(zero > 0){
-                need = max(need + 1, zero);
+
+        int n=s.length();
+        int flag=1;
+        int cc=0;
+        while(1){
+            flag=1;
+            for(int i=0;i<n-1;i++)
+            {
+                if(s[i]=='0' && s[i+1]=='1'){
+                    s[i]='1';
+                    s[i+1]='0';
+                    i++;
+                    flag=0;
+
+                }
+
             }
+            
+            if(flag==1){
+                break;
+            }
+            cc++;
         }
-        return need;
+        return cc;
+            
     }
 };
