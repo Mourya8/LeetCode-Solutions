4fdc4988388f8f3056235c59a083421ae234295f
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-07-14 01:25:10 -0400

    C++ Solution
            Submission Statistics:
            Question #: 3501
            Language: C++
            Runtime: 559 ms
            Runtime percentile: 25.00
            Memory: 260.896 MB
            Memory percentile: 75.00

---
+++ 3501. Delete Nodes From Linked List Present in Array/Solution.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        map<int,bool> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]=1;
+        }
+        while(head!=NULL&&m.find(head->val)!=m.end())
+        {
+            head=head->next;
+        }
+        ListNode* temp=head;
+        while(temp!=NULL)
+        {
+            if(temp->next)
+            {
+                if(m.find(temp->next->val)!=m.end())
+                {
+                    temp->next=temp->next->next;
+                }
+                else
+                {
+                    temp=temp->next;
+                }
+            }
+            else temp=temp->next;
+        }
+        return head;
+    }
+};
