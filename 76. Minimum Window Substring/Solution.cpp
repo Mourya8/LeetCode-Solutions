0b0d5803fc6ea51ae1d7396876ca10ceedf75444
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-07-20 02:29:40 -0400

    C++ Solution
            Submission Statistics:
            Question #: 76
            Language: C++
            Runtime: 11 ms
            Runtime percentile: 76.00
            Memory: 10.004 MB
            Memory percentile: 72.10

--- 76. Minimum Window Substring/Solution.cpp
+++ 76. Minimum Window Substring/Solution.cpp
@@ -1,69 +1,35 @@
 class Solution {
 public:
-    string minWindow(string str, string t) {
-        if(str.size() ==0) return "";
-    unordered_map <char, int> m;
-    int l=0; 
-    int r=0;
-    unordered_map <char,int> a;
-    for(int i=0;i< t.size();i++)
-    {
-        if(a.find(t[i])!=a.end()) a[t[i]]++;
-        else a[t[i]]=1;
-    }
-    string retStr="";
-    int start=0,minLength=0;
-    while(l<str.size())
-    {
-        bool x=1;
-        for(auto &it: a)
-        {
-            if(m.find(it.first)!=m.end())
-            {
-                //cout<< m[it.first]<<" "<<it.second<<endl;
-                if(m[it.first]<it.second) 
-                {
-                    
-                    x=0;
-                    break;
+    string minWindow(string s, string t) {
+        if (s.empty() || t.empty() || s.length() < t.length()) {
+            return "";
+        }
+
+        std::vector<int> map(128, 0);
+        int count = t.length();
+        int start = 0, end = 0, minLen = INT_MAX, startIndex = 0;
+        /// UPVOTE !
+        for (char c : t) {
+            map[c]++;
+        }
+
+        while (end < s.length()) {
+            if (map[s[end++]]-- > 0) {
+                count--;
+            }
+
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
+                }
+
+                if (map[s[start++]]++ == 0) {
+                    count++;
                 }
             }
-            else
-            {
-                x=0;
-                break;
-            }
         }
-        if(x)
-        {
-            //cout<<str.substr(l,r-l)<<" test"<<endl;
-            if(retStr=="") 
-            {
-                retStr=str.substr(l,r-l);
-                start=l; minLength=r-l;
-            }
-            else{
-                if(r-l<minLength)
-                {
-                    start=l; minLength=r-l;
-                }
-            }
-            m[str[l]]--;
-            l++;
-        }
-        else{
-            if(r>=str.size()) break;
-            if(m.find(str[r])!=m.end())
-            {
-                m[str[r]]++;
-            }
-            else
-            {
-                m[str[r]]=1;
-            }
-            r++;
-        }
-    }
-    return str.substr(start,minLength);
+
+        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen);
     }
 };
