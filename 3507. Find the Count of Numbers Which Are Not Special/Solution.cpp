a6f1d2134291a7c906df423f1df9bea70f5db4b0
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-08-03 17:09:52 -0400

    C++ Solution
            Submission Statistics:
            Question #: 3507
            Language: C++
            Runtime: 75 ms
            Runtime percentile: 46.12
            Memory: 9.688 MB
            Memory percentile: 55.21

--- 3507. Find the Count of Numbers Which Are Not Special/Solution.cpp
+++ 3507. Find the Count of Numbers Which Are Not Special/Solution.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    bool isPrime(int N) {
-    if (N <= 1) {
-        return false;
-    }
+//     bool isPrime(int N) {
+//     if (N <= 1) {
+//         return false;
+//     }
 
-    for (int i = 2; i <= N/2; i++) {
+//     for (int i = 2; i <= N/2; i++) {
       
-        if (N % i == 0) {
-            return false;
-        }
-    }
+//         if (N % i == 0) {
+//             return false;
+//         }
+//     }
 
-    return true;
-}
+//     return true;
+// }
     int nonSpecialCount(int l, int r) {
         int total = r-l+1;
         int pd =0;
         int x =sqrt(l);
         int y= sqrt(r);
+        int limit = (int)(sqrt(r));
+        vector<bool> v(limit+1, true);
+        v[0] = v[1] = false;
+
+        for (int i=2; i*i<=limit; i++) {
+            if (v[i]) {
+                for (int j=i*i; j<=limit; j+=i) {
+                    v[j] = false;
+                }
+            }
+        }
         for(int i=x;i<=y;i++)
         {
             int z = i*i;
-            if(isPrime(i)&&(z)>=l&& z<=r)
+            if(v[i]&&(z)>=l&& z<=r)
             {
                 cout<<i<<endl;
                 pd++;
