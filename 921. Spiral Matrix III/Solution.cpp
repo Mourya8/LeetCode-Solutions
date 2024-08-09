63aedbe292842efef7efe6456f0a73a29a1ff926
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-08-09 10:27:14 -0400

    C++ Solution
            Submission Statistics:
            Question #: 921
            Language: C++
            Runtime: 7 ms
            Runtime percentile: 86.72
            Memory: 13.932 MB
            Memory percentile: 12.28

--- 921. Spiral Matrix III/Solution.cpp
+++ 921. Spiral Matrix III/Solution.cpp
@@ -3,9 +3,7 @@ public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         int total = rows* cols;
         vector <vector<int>> r;
-        vector<vector<int>> t;
         r.push_back({rStart,cStart});
-        t=r;
         vector<vector<int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
         int counter = 2;
         int length = 1;
@@ -21,12 +19,12 @@ public:
             d=d%4;
             while(tempLen>0)
             {
-                vector<int> temp = t[t.size()-1];
-                t.push_back({temp[0]+directions[d][0],temp[1]+directions[d][1]});
-                if(temp[0]+directions[d][0]>=0 && temp[0]+directions[d][0]<rows && temp[1]+directions[d][1]>=0 && temp[1]+directions[d][1]<cols)
+                rStart = rStart+directions[d][0];
+                cStart = cStart+directions[d][1];
+                if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols)
                 {
                     //cout<<temp[0]+directions[d][0]<<" "<<temp[1]+directions[d][1]<<endl;
-                    r.push_back({temp[0]+directions[d][0],temp[1]+directions[d][1]});
+                    r.push_back({rStart,cStart});
                 }
                 if(r.size()==total) return r;
                 tempLen--;
