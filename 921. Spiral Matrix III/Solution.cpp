1ce5c7395a724112560a5244923afa369e541496
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-08-09 10:23:14 -0400

    C++ Solution
            Submission Statistics:
            Question #: 921
            Language: C++
            Runtime: 59 ms
            Runtime percentile: 5.01
            Memory: 38.348 MB
            Memory percentile: 5.26

--- 921. Spiral Matrix III/Solution.cpp
+++ 921. Spiral Matrix III/Solution.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int totalCells = rows * cols;
-        int leftLimit = cStart, rightLimit = cStart;
-        int upLimit = rStart, downLimit = rStart;
-        vector<vector<int>> visitedOrder(totalCells, vector<int>(2));
-        int visitedIndx = 0;
-        int currR = rStart, currC = cStart;
-        visitedOrder[visitedIndx++] = {currR, currC};
-        while(visitedIndx < totalCells) {
-            while(currC < rightLimit + 1) {
-                currC++;
-                if(isInGrid(currR, currC, rows, cols) && visitedIndx < totalCells) {
-                    visitedOrder[visitedIndx++] = {currR, currC};
-                }
+        int total = rows* cols;
+        vector <vector<int>> r;
+        vector<vector<int>> t;
+        r.push_back({rStart,cStart});
+        t=r;
+        vector<vector<int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        int counter = 2;
+        int length = 1;
+        int d=0;
+        while(r.size()<=total)
+        {
+            if(counter==0)
+            {
+                counter=2;
+                length+=1;
             }
-            rightLimit++;
-            while(currR < downLimit + 1) {
-                currR++;
-                if(isInGrid(currR, currC, rows, cols) && visitedIndx < totalCells) {
-                    visitedOrder[visitedIndx++] = {currR, currC};
+            int tempLen=length;
+            d=d%4;
+            while(tempLen>0)
+            {
+                vector<int> temp = t[t.size()-1];
+                t.push_back({temp[0]+directions[d][0],temp[1]+directions[d][1]});
+                if(temp[0]+directions[d][0]>=0 && temp[0]+directions[d][0]<rows && temp[1]+directions[d][1]>=0 && temp[1]+directions[d][1]<cols)
+                {
+                    //cout<<temp[0]+directions[d][0]<<" "<<temp[1]+directions[d][1]<<endl;
+                    r.push_back({temp[0]+directions[d][0],temp[1]+directions[d][1]});
                 }
+                if(r.size()==total) return r;
+                tempLen--;
             }
-            downLimit++;
-            while(currC > leftLimit - 1) {
-                currC--;
-                if(isInGrid(currR, currC, rows, cols) && visitedIndx < totalCells) {
-                    visitedOrder[visitedIndx++] = {currR, currC};
-                }
-            }
-            leftLimit--;
-            while(currR > upLimit - 1) {
-                currR--;
-                if(isInGrid(currR, currC, rows, cols) && visitedIndx < totalCells) {
-                    visitedOrder[visitedIndx++] = {currR, currC};
-                }
-            }
-            upLimit--;
+            d++;
+            counter--;
         }
-        return visitedOrder;
-    }
-
-private:
-    bool isInGrid(int r, int c, int rows, int cols) {
-        return r > -1 && c > -1 && r < rows && c < cols;
+        return r;
     }
 };
