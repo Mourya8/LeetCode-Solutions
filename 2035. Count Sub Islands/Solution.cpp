0dade0dc0063b23d9648375a555493227ff27427
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-08-30 18:00:38 -0400

    C++ Solution
            Submission Statistics:
            Question #: 2035
            Language: C++
            Runtime: 238 ms
            Runtime percentile: 35.28
            Memory: 93.616 MB
            Memory percentile: 69.61

--- 2035. Count Sub Islands/Solution.cpp
+++ 2035. Count Sub Islands/Solution.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> d = {{0,1},{0,-1},{1,0},{-1,0}};
-    void mapIsland(vector<vector<int>>& grid, map <pair<int,int>, bool> &m, queue <pair<int,int>> &q)
-    {
-        int x, y ;
-        while(!q.empty())
-        {
-            x = q.front().first;
-            y = q.front().second;
-            m[{x,y}]=1;
-            q.pop();
-            for(int i=0;i<d.size();i++)
-            {
-                if(x+d[i][0]>=0 && x+d[i][0]<grid.size()  && y + d[i][1] >=0 && y + d[i][1]<grid[0].size())
-                {
-                    if(grid[x+d[i][0]][y+d[i][1]]==1)
-                    {
-                        grid[x+d[i][0]][y+d[i][1]]=2;
-                        q.push({x+d[i][0],y+d[i][1]});
-                    }
-                }
-            }
-        }
-        
-    }
 
     void allExist(vector<vector<int>>& grid, vector<vector<int>>& grid1, queue <pair<int,int>> &q, bool &b)
     {
@@ -50,22 +27,8 @@ public:
     }
 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        map <pair<int,int>, bool> m;
-        vector<pair<int,int>> v;
         queue <pair<int,int>> q;
         int count =0;
-        // for(int i=0;i<grid1.size();i++)
-        // {
-        //     for(int j=0;j<grid1[0].size();j++)
-        //     {
-        //         if(grid1[i][j]==1)
-        //         {
-        //             q.push({i,j});
-        //             grid1[i][j]=2;
-        //             mapIsland(grid1,m,q);
-        //         }
-        //     }
-        // }
         bool b = 1;
 
         for(int i=0;i<grid2.size();i++)
@@ -81,7 +44,6 @@ public:
                     allExist(grid2,grid1,q,b);
                     if(b) 
                     {
-                        //cout<< i <<" "<<j<<endl;
                         count++;
                     }
                 }
