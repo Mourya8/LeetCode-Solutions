a41cefabfaad6cf60595c1e026272eaf4f9dd204
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-09-12 17:57:01 -0400

    C++ Solution
            Submission Statistics:
            Question #: 3
            Language: C++
            Runtime: 15 ms
            Runtime percentile: 54.09
            Memory: 12.848 MB
            Memory percentile: 41.34

--- 3. Longest Substring Without Repeating Characters/Solution.cpp
+++ 3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -1,53 +1,31 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         if(s=="")
-//         {
-//             return 0;
-//         }
-//         int l=0;
-//         int r=1;
-//         map<char,int> m;
-//         m[s[l]]=l;
-//         int length=1;
-//         int maxl=1;
-//         while(r<s.size())
-//         {
-//             if(m.find(s[r])!=m.end())
-//             {
-//                 length=1;
-//                 l=max(l,m[s[r]]+1);
-//                // cout<<l<<" "<<s[r]<<endl;
-//                 m.erase(m[s[l]]);
-//                 m[s[l]]=l;
-//             }
-//                 m[s[r]]=r;
-//                 length=r-l+1;
-//                 //cout<<l<<" "<<r<<endl;
-//                 maxl=max(maxl,length);
-//                 r++;
-//         }
-//         return maxl;
-
-//     }
-// };
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char,int> m;
-        int l=0,r=0;
-        int length=0;
-        int n=s.size();
-        while(r<n)
+        if(s.size()<=1) return s.size();
+        int maxL = INT_MIN;
+        map<char,int> x;
+        int l=0;
+        int r = 0;
+        while(r<s.size())
         {
-            if(m.find(s[r])!=m.end())
+            if(x.find(s[r])==x.end())
             {
-                l=max(l,m[s[r]]+1);
+                
+                x[s[r]]=r;
             }
-            m[s[r]]=r;
-            length=max(length,r-l+1);
+            else
+            {
+                
+                l=max(l,x[s[r]]+1);
+                x[s[r]]=r;
+                
+            }
+            //cout<<l<<" "<<r<<endl;
+            maxL = max(maxL,r-l+1);
             r++;
         }
-        return length;
+        //cout<<l<<" "<<r<<endl;
+        maxL = max(maxL,r-l);
+        return maxL;
     }
 };
