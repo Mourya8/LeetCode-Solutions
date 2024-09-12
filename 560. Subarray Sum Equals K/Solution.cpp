a97591cbba8113871be12967242a50519e019f0b
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-09-12 18:41:10 -0400

    C++ Solution
            Submission Statistics:
            Question #: 560
            Language: C++
            Runtime: 64 ms
            Runtime percentile: 74.25
            Memory: 50.72 MB
            Memory percentile: 32.32

---
+++ 560. Subarray Sum Equals K/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        int sum = 0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(sum==k) count++;
+            int temp=sum-k;
+            count+=m[temp];
+
+            m[sum]++;
+        }
+
+        return count;
+    }
+};
