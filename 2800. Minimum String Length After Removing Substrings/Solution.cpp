d435b9ac8254eddc637c3df04c89bd4f11866a91
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2024-10-07 10:43:55 -0400

    C++ Solution
            Submission Statistics:
            Question #: 2800
            Language: C++
            Runtime: 8 ms
            Runtime percentile: 42.58
            Memory: 11.436 MB
            Memory percentile: 72.37

---
+++ 2800. Minimum String Length After Removing Substrings/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='B')
+            {
+                if(!st.empty()&& st.top()=='A')
+                {
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+            else if(s[i]=='D')
+            {
+                if(!st.empty()&& st.top()=='C')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
