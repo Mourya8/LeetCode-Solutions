6175e9fd02561931153da3b1113ed0631e4e1e32
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2025-01-01 23:17:10 -0500

    C++ Solution
            Submission Statistics:
            Question #: 882
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 63.356 MB
            Memory percentile: 60.72

---
+++ 882. Peak Index in a Mountain Array/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0;
+        int r = arr.size();
+        int mid = l+(r-l)/2;
+        while(1)
+        {
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]) break;
+            if(arr[mid-1]<arr[mid])
+            {
+                l=mid;
+            }
+            else
+            {
+                r=mid;
+            }
+            mid = l+(r-l)/2;
+        }
+        return mid;
+    }
+};
