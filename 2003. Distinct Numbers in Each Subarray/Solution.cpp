ec40f45f07a1a05e13e512a89463de85e4c97ca4
Sai Mourya Buchi <44705866+Mourya8@users.noreply.github.com>
2025-02-04 00:59:29 -0500

    C++ Solution
          Submission Statistics:
          Question #: 2003
          Language: C++
          Runtime: 438 ms
          Runtime percentile: 5.06
          Memory: 158.404 MB
          Memory percentile: 5.31

---
+++ 2003. Distinct Numbers in Each Subarray/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> distinctNumbers(vector<int>& nums, int k) {
+        map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<k;i++)
+        {
+            m[nums[i]]++;
+        }
+        ans.push_back(m.size());
+        for(int i=k;i<nums.size();i++)
+        {
+            m[nums[i-k]]--;
+            m[nums[i]]++;
+            if(m[nums[i-k]]==0) m.erase(nums[i-k]);
+            ans.push_back(m.size());
+        }
+        return ans;
+    }
+};
